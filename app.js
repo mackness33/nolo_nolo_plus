30a0bc19a5d8275505da504f68017fc0578e6d5e
David Mack <david.mack@studio.unibo.it>
2021-10-07 17:43:22 +0200

    :poop: First test of mongoose and Mongodb on expressjs

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var kittenRouter = require('./routes/mongo_ex_url');
 
 var app = express();
 
@@ -22,11 +24,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// //Set up default mongoose connection
+// var mongoDB = 'mongodb://127.0.0.1:27017/test';
+// mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+//
+// //Get the default connection
+// var db = mongoose.connection;
+//
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// const fluffy = new Kitten({ name: 'fluffy' });
+// fluffy.speak(); // "Meow name is fluffy"
+//
+// await fluffy.save();
+// fluffy.speak();
+//
+// const kittens = await Kitten.find();
+// console.log(kittens);
+//
+// await Kitten.find({ name: /^fluff/ });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/kitten', kittenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log("Error occured");
   next(createError(404));
 });
 
