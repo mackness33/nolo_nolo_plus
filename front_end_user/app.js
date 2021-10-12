c96b787a406a8f7cb49c3af944cb24cdfdc790de
fabio.mirza <mapokko@hotmail.it>
2021-10-12 15:26:06 +0200

    improved date and time selection

--- front_end_user/app.js
+++ front_end_user/app.js
@@ -1,7 +1,36 @@
 
+const dateInput = document.querySelector('#bookDate .form-control')
+const timeInput = document.querySelector('#bookTime .form-select');
+
+//console.log(timeInput.removeChild(timeInput.querySelector('option')));
+
+dateInput.addEventListener('change', (e) => {
+    let today = new Date();
+    let selectedDate = new Date(e.currentTarget.value);
+
+    if(today.toLocaleDateString() ==  selectedDate.toLocaleDateString()){
+        timeInput.querySelectorAll('option').forEach(element => {
+            if(element.value <= today.getHours()){
+                element.setAttribute('disabled', 'true');
+            }
+            else{
+                element.removeAttribute('disabled');
+                element.setAttribute('selected', '');
+            }
+        });
+    }
+    else{
+        timeInput.querySelectorAll('option').forEach(element => {
+            element.removeAttribute('disabled');
+        });
+    }
+});
 
 window.addEventListener('DOMContentLoaded', ()=>{
-    let dateInput = document.getElementById('bookDate').querySelector('.form-control');
-    
-    dateInput.setAttribute('min', (new Date()).toISOString().split('T')[0]);
+    let today = new Date();
+
+    dateInput.setAttribute('min', today.toISOString().split('T')[0]);
+    today.setDate(today.getDate() + 90);
+    dateInput.setAttribute('max', ((today.toISOString()).split('T')[0]));
 });
+
