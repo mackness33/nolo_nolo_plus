755e9946153222e1e9829bbacf8056c3b50b9575
David Mack <david.mack@studio.unibo.it>
2021-10-15 15:05:00 +0200

    :fire: Test useless

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/pedalo', pedaloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log("Error occured, damn boy");
+  console.log("Error occured");
   next(createError(404));
 });
 
