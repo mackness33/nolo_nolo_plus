1c6ff561274eb62620f4fa0d5f2d939b9c921dad
fabio <fabiomirza@outlook.com>
2021-12-21 22:43:04 +0100

    moved routes auth control

--- back-office/routes/user.js
+++ back-office/routes/user.js
@@ -3,156 +3,77 @@ const logger = require("../../logger.js");
 const userModel = require("../../services/mongo/schema/user");
 const emplModel = require("../../services/mongo/schema/employee");
 const SessionService = require("../../services/auth");
+const pedService = require("../../services/pedalo");
 const helper = require("../../services/mongo/base.js");
 
-router.get(
-  "/getOne",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    if (req.query.mail) {
-      var user = await helper.getOne(userModel, req);
-      user = helper.format(user);
-      for (let i = 0; i < user.feedback.length; i++) {
-        user.feedback[i].emplCode = helper.format(
-          user.feedback[i].emplCode,
-          true
-        );
-      }
-    } else {
-      var user = await helper.getOne(userModel, req);
-      for (let i = 0; i < user.length; i++) {
-        user[i] = helper.format(user[i]);
-      }
+router.use((req, res, next) => {
+  SessionService.authorization(
+    req,
+    res,
+    next,
+    "/nnplus/logout",
+    "/nnplus/login",
+    2
+  );
+});
+
+router.get("/getOne", async (req, res, next) => {
+  if (req.query.mail) {
+    var user = await helper.getOne(userModel, req);
+    user = helper.format(user);
+    for (let i = 0; i < user.feedback.length; i++) {
+      user.feedback[i].emplCode = helper.format(
+        user.feedback[i].emplCode,
+        true
+      );
     }
-
-    res.send(user);
-  }
-);
-
-router.post(
-  "/setOne",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    await helper.setOne(userModel, req);
-    res.send("ahhh done");
-  }
-);
-
-router.get(
-  "/all",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    const users = await helper.getAll(userModel);
-    for (let i = 0; i < users.length; i++) {
-      users[i] = helper.format(users[i], false);
+  } else {
+    var user = await helper.getOne(userModel, req);
+    for (let i = 0; i < user.length; i++) {
+      user[i] = helper.format(user[i]);
     }
-    res.send(users);
   }
-);
 
-router.post(
-  "/add",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    await helper.add(userModel, req);
-    res.send("all cool");
-  }
-);
+  res.send(user);
+});
 
-router.post(
-  "/feed",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    const emp = await helper.getOne(emplModel, req.session.mail);
-    req.query = { mail: req.body.userMail };
-    const user = await helper.getOne(userModel, req);
-    user.feedback.push({
-      date: req.body.date,
-      text: req.body.text,
-      emplCode: emp._id,
-    });
-    await user.save();
-    res.send("bene bene");
-  }
-);
+router.post("/setOne", async (req, res, next) => {
+  await helper.setOne(userModel, req);
+  res.send("ahhh done");
+});
 
-router.post(
-  "/deleteOne",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    res.send(await helper.deleteOne(userModel, req.body.mail));
+router.get("/all", async (req, res, next) => {
+  const users = await helper.getAll(userModel);
+  for (let i = 0; i < users.length; i++) {
+    users[i] = helper.format(users[i], false);
   }
-);
+  res.send(users);
+});
 
-router.get(
-  "/checkExist",
-  (req, res, next) => {
-    SessionService.authorization(
-      req,
-      res,
-      next,
-      "/nnplus/logout",
-      "/nnplus/login",
-      2
-    );
-  },
-  async (req, res, next) => {
-    res.send(await helper.checkExist(userModel, req.query.mail));
-  }
-);
+router.post("/add", async (req, res, next) => {
+  await helper.add(userModel, req);
+  res.send("all cool");
+});
+
+router.post("/feed", async (req, res, next) => {
+  const emp = await helper.getOne(emplModel, req.session.mail);
+  req.query = { mail: req.body.userMail };
+  const user = await helper.getOne(userModel, req);
+  user.feedback.push({
+    date: req.body.date,
+    text: req.body.text,
+    emplCode: emp._id,
+  });
+  await user.save();
+  res.send("bene bene");
+});
+
+router.post("/deleteOne", async (req, res, next) => {
+  res.send(await helper.deleteOne(userModel, req.body.mail));
+});
+
+router.get("/checkExist", async (req, res, next) => {
+  res.send(await helper.checkExist(userModel, req.query.mail));
+});
 
 module.exports = router;
