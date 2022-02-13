a5463783ea024055e09a15f18f5db3b505d67fbf
David Mack <david.mack@studio.unibo.it>
2022-02-13 14:41:07 +0100

    :art: IMproving css

--- back-office/manager/dashboard/src/App.vue
+++ back-office/manager/dashboard/src/App.vue
@@ -4,43 +4,91 @@
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+  <div class="general">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark br-primary d-flex align-items-center">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- navbar content -->
+      <div class="container-fluid" id="nav-container">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <a class="navbar-brand d-flex align-items-center" href="#" aria-hidden="true" tabindex="-1">
+        <!-- navbar logo -->
+          <img src="@/assets/logo.png" class="logo">
+        </a>
+
+        <!-- navbar toggle -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navElements"
+        aria-controls="navElements" aria-expanded="false" aria-label="Mostra e nascondi navigazione">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- navbar navigation -->
+      <div class="collapse navbar-collapse" id="navElements">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="nav-list">
+          <li class="nav-item">
+            <a class="link active-nav" href="/nnplus/home" aria-label="Pagina Clienti" aria-current="page">Clienti</a>
+          </li>
+          <li class="nav-item">
+            <a class="link" href="/nnplus/home/inventory" aria-label="Pagina Inventario">Inventario</a>
+          </li>
+          <li class="nav-item">
+            <a class="link" href="/nnplus/home/booking" aria-label="Pagina Gestione">Noleggi</a>
+          </li>
+          <li class="nav-item">
+            <a class="link" href="/nnplus/dashboard/" aria-label="Pagina Gestione">Dashboards</a>
+          </li>
+          <li class="nav-item">
+            <a class="link" href="/nnplus/logout" aria-label="Effettua il log out">Log Out</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </header>
+  </nav>
 
-  <RouterView />
+
+
+  <!-- main content -->
+  <main>
+
+    <!-- add and search customer -->
+    <div class="container-fluid" id="main-container">
+      <header>
+        <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+
+        <div class="wrapper">
+          <HelloWorld msg="You did it!" />
+
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+      </header>
+
+      <RouterView />
+    </div>
+  </main>
+  </div>
 </template>
 
 <style>
-@import '@/assets/base.css';
 @import '@/assets/nav.css';
 @import '@/assets/users.css';
 
+.general {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
 #app {
-  max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
 
   font-weight: normal;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
 .logo {
   display: block;
-  margin: 0 auto 2rem;
 }
 
 a,
@@ -56,66 +104,16 @@ a,
   }
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
+@media (max-width: 1024px) {
   body {
     display: flex;
     place-items: center;
   }
 
   #app {
-    display: grid;
+    display: flex;
     grid-template-columns: 1fr 1fr;
     padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
