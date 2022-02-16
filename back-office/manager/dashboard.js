1353028a00d5727711ba0b76c6946a1bfbbc5939
fabio <fabiomirza@outlook.com>
2022-02-16 12:22:17 +0100

    working 2

--- back-office/manager/dashboard.js
+++ back-office/manager/dashboard.js
@@ -10,6 +10,7 @@ const session = require("express-session");
 // const homeRouter = require("./routes/home");
 const userRoute = require("./dashboard/routes/user");
 const invRoute = require("./dashboard/routes/inventory");
+const bookingRoute = require("./dashboard/routes/booking");
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, "dashboard/src/assets")));
 
 app.use("/user", userRoute);
 app.use("/inv", invRoute);
+app.use("/booking", bookingRoute);
 
 // app.use("/", homeRouter);
 app.get("/prova", async (req, res, next) => {
