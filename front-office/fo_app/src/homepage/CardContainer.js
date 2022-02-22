3d732f7b403629c3476bba052a344b3e082961bd
fabio <fabiomirza@outlook.com>
2022-02-22 20:58:50 +0100

    show favourites first in front office

--- front-office/fo_app/src/homepage/CardContainer.js
+++ front-office/fo_app/src/homepage/CardContainer.js
@@ -13,14 +13,23 @@ import { ClassNames } from "@emotion/react";
 
 const CardContainer = () => {
   const mobile = useMediaQuery("(max-width: 768px)");
+  const [localComps, setLocalComps] = React.useState();
   const { computers, setComputers } = React.useContext(ComputerContext);
   const { globalUser, setGlobalUser } = React.useContext(NetworkContext);
   const [user, setUser] = React.useState();
   React.useEffect(async () => {
     const res = await identity();
     if (res.success) {
-      console.log("PIACENTI");
-      console.log(res);
+      if (computers) {
+        let temp = JSON.parse(JSON.stringify(computers));
+        let cop = temp.filter((x) => res.payload.favourites.includes(x._id));
+
+        const filtered = computers.filter(
+          (comp) => !res.payload.favourites.includes(comp._id)
+        );
+
+        setLocalComps([...cop, ...filtered]);
+      }
       setUser(res.payload);
     } else {
       setUser(null);
@@ -41,7 +50,7 @@ const CardContainer = () => {
         ]}
         disableGutters
       >
-        {computers?.map((computer) => {
+        {localComps?.map((computer) => {
           const chosen = user ? user.favourites.includes(computer._id) : false;
           return (
             <Computercard computer={computer} user={user} chosen={chosen} />
